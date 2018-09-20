34dde0bfa76e3f4ea3ebaaad6873bbb93ba31b96
VyronLee <lwz_jz@hotmail.com>
2018-09-20 13:47:19 +0800

    Fix build scripts.

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 
-IMAGES=($(ls -d */ | xargs basename))
+IMAGE_NAMES=($(find . -type d -not -wholename "./.*" -not -wholename "." -exec basename {} \;))
 
-for NAME in ${IMAGES[@]}; do
-    echo "building image: $NAME"
+for NAME in ${IMAGE_NAMES[@]}; do
+    echo "building image:" $NAME
     cd $NAME
     docker build -t $DOCKER_PASS/$NAME:latest . || { echo "build failed!"; exit 1; }
     docker push $DOCKER_PASS/$NAME
